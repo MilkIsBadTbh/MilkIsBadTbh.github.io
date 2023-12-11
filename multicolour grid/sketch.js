eaf3c70f57f40873eea6bf3e7a9e82b01dce957e
MilkIsBadTbh <145151536+MilkIsBadTbh@users.noreply.github.com>
2023-12-11 12:06:12 -0600

    decc 1

--- multicolour grid/sketch.js
+++ multicolour grid/sketch.js
@@ -1,9 +1,11 @@
-// Project Title
-// Your Name
-// Date
-//
+// Multi Coloured Grid
+// Terron Adamus
+// Nov 9th, 2023
+// 
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - Creating a Multi Coloured Grid
+
+// --------------------------------------------------
 
 // Global
 let rectSize = 30;
@@ -12,20 +14,40 @@ function setup() {
   stroke(75);
   strokeWeight(2);
   drawGrid();
+  document.addEventListener("contextmenu", event => event.preventDefault());
 }
 
-function getColour(){
+function mousePressed() {
+  if (mouseButton === LEFT) { // Checking if left mouse is clicked,-
+    rectSize -= 10;           // -If so, make the rectangles smaller
+    if (rectSize <= 10){      // Prevents our rectangles from getting too small and crashing the program
+      rectSize = 10;
+    }
+
+  }
+  if (mouseButton === RIGHT) { // Checks if right mouse is clicked,-
+    rectSize += 10;            // - if so, makes rectangles larger
+  }
+  drawGrid();
+}
+
+function keyPressed(){        // If any key is pressed, this will randomize the grid colours
+  drawGrid();
+}
+
+
+
+function getColour() {       // Creates a rabdom RGB colour per square
   let r = floor(random(255));
   let g = floor(random(255));
   let b = floor(random(255));
-  return color(r,g,b);
+  return color(r, g, b);
 }
-function drawGrid(){
-  for(let x = 0; x < width; x+=rectSize){
-    for(let y = 0; y < height; y +=rectSize){
+function drawGrid() {        // Creates the grid by adding onto other rectangles
+  for (let x = 0; x < width; x += rectSize) {
+    for (let y = 0; y < height; y += rectSize) {
       fill(getColour());
       rect(x, y, rectSize, rectSize);
     }
   }
-
 }
