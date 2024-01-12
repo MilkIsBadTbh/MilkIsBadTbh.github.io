105ff7f477eceb6288f248e86e3aced05f8e6487
MilkIsBadTbh <145151536+MilkIsBadTbh@users.noreply.github.com>
2024-01-12 12:14:09 -0600

    dinoDead;(
    collision detection is worked out, maybe a bit of tweaking to go but base is done. Dino now displays the dead dino image when a collision is detected, working on getting the "game over" to display when dino dies.

--- Capstone Project/sketch.js
+++ Capstone Project/sketch.js
@@ -11,9 +11,10 @@ let dinoImages = [];
 let cactiImages = [];
 let currentImage = 0;
 let cactiSpeed = 8;
+let stayinAlive = true;
 
 let ySpeed;
-let gravity; 
+let gravity;
 let dino;
 let cactus1;
 
@@ -22,6 +23,7 @@ function preload() {
   dinoImages.push(loadImage("assets/standing dino.png"));
   dinoImages.push(loadImage("assets/left foot up dino.png"));
   dinoImages.push(loadImage("assets/right foot up dino.png"));
+  dinoImages.push(loadImage("assets/dead dino.png"));
   cactiImages.push(loadImage("assets/cactus1.png"));
 }
 
@@ -29,17 +31,24 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   dino = new Dino(width * 0.15, height / 1.15);
-  cactus1 = new Cacti(width, height/1.135);
+  cactus1 = new Cacti(width, height / 1.135);
 }
 
 function draw() {
   background(220);
   drawGround();
-  dino.move();
+
   dino.display();
   cactus1.display();
-  cactus1.move();
+
   collision();
+  if (stayinAlive === true) {
+    dino.move();
+    cactus1.move();
+  }
+  else {
+
+  }
 }
 
 function drawGround() {
@@ -82,22 +91,27 @@ class Dino {
   }
   display() {
     print(currentImage);
-    if (this.jumping === true) {
-      currentImage = 0;
-    }
-    else {
-      if (currentImage === 0) { currentImage = 1; }
+    if (stayinAlive === true) {
+      if (this.jumping === true) {
+        currentImage = 0;
+      }
+      else {
+        if (currentImage === 0) { currentImage = 1; }
 
-      if (frameCount % 10 === 0) {
-        if (currentImage === 1) {
-          currentImage = 2;
+        if (frameCount % 10 === 0) {
+          if (currentImage === 1) {
+            currentImage = 2;
+          }
+          else {
+            currentImage = 1;
+          }
         }
-        else {
-          currentImage = 1;
-        }
+
       }
 
+
     }
+
     image(dinoImages[currentImage], this.position.x, this.position.y);
     //print(this.position.y);
   }
@@ -106,19 +120,21 @@ class Cacti {
   constructor(x, y) {
     this.position = createVector(x, y); this.s = 20;
   }
-  display(){
-    image(cactiImages[0],this.position.x, this.position.y);
+  display() {
+    image(cactiImages[0], this.position.x, this.position.y);
   }
-  move(){
+  move() {
     this.position.x -= cactiSpeed;
-    if (this.position.x < 30){
+    if (this.position.x < 30) {
       this.position.x = width;
     }
   }
 }
 
-function collision(){
-  if(dist(dino.position.x,dino.position.y,cactus1.position.x,cactus1.position.y) < 100){
+function collision() {
+  if (dist(dino.position.x, dino.position.y, cactus1.position.x, cactus1.position.y) < 100) {
     print("collide!");
+    currentImage = 3;
+    stayinAlive = false;
   }
 }
